7b16447584eb53e2e1bb316b2d0156c99cb8db10
patdaburu <pat@daburu.net>
2019-09-21 06:10:45 -0500

    Removed Click requirements.

--- setup.py
+++ setup.py
@@ -51,7 +51,6 @@ setup(
         # Here are a couple of examples...
         # 'numpy>=1.13.3,<2',
         # 'measurement>=1.8.0,<2'
-        'click>=7.0,<8'
     ],
     entry_points="""
     [console_scripts]
